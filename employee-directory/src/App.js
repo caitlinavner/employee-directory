598992a008ed3c9f0c425ac04515681819b2e48f
cavner <katieavner@gmail.com>
2020-03-31 17:36:58 -0700

    created tables and functions

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Table from './components/Table';
 
 function App() {
   return (
     <div className="App">
       <Header /> 
+      <Table />
       {/* only needs open and close if something is inside components, this will be like a wrapper for the page*/}
     </div>
   );
--- employee-directory/src/components/Table.js
+++ employee-directory/src/components/Table.js
@@ -12,7 +12,21 @@ export default class Table extends Component {
             users: [{}], 
             //don't use a comma at the end otherwise it'll keep looking
             //sort users and put them in a new array
-            sortedUsers: [{}]
+          sortedUsers: [{}],
+          order: "ascending",
+            //function and pass in 
+          //key value pairs has space after colon
+          tableHeadings: [{ name: "Name" }, { name: "Age" }, { name: "DOB" } ],
+          handleOrder: age => {
+            if (this.state.order === "ascending") {
+              this.setState({ order: "descending" })
+            } else {
+              this.setState({ order: "ascending" });
+            }
+            console.log("you clicked me");
+            //set up comparison function and then sort based on age
+
+          }
 
         }
     }
@@ -26,17 +40,35 @@ export default class Table extends Component {
                 sortedUsers: APIresults.data.results
                 //build table and get results to display
                 //this.state.users to show up on page
-
+                //map function used when info inside db will be changing and it will need to be updated and displayed
             })
+          console.log(this.state.users);
+          //sort by one category and filter by one property
         })
     }
 
   render() {
     return (
-      <div className="table">
-            {/* sorting requires State to track things dynamically */}
+      <div className="table-container">
+        {/* sorting requires State to track things dynamically */}
+        <table className="table">
+          <thead>
+            <tr>
+              {/* //when calling a function you need parenthesis right after. no spaces */}
+              {this.state.tableHeadings.map(({ name }) => {
+                return (
+                  <th className="col" key={name} onClick={() => {this.state.handleOrder(name)}}>
+                   {name} 
+                  </th>
+                )
+              } )}
+            </tr>
+          </thead>
+
+        </table>
             
       </div>
     );
   }
 }
+//body of table will have same format and needs to use map-don't need onclick-use td-use bootstrap to avoid css
